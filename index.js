ba1daca4afe740c3ff4fb0b0397de0c8f897a953
Farida <faridah.absi@gmail.com>
2018-09-16 09:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  let newarray = [];
+  
+  for (let element of array){
+    newarray.push(element.toLowerCase());
+  }
+  return newarray;
+}
+
+function nameToAttributes(drivers){
+let arrObj =[];
+for (let x of drivers ){
+var fname = x.split(" ")[0];
+var lname = x.split(" ")[1];
+
+  arrObj.push({firstName: fname,lastName: lname });
+}
+  return  arrObj;
+}
+
+attributesToPhrase(drivers){}
+
