e02af9c7c064fbbe4ed463e7b107863aa2dc1cf3
Farida <faridah.absi@gmail.com>
2018-09-16 09:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function attributesToPhrase(drivers){
   
   for (let x of drivers ){
     
-   arrayOBJ.push(`+${x.name}+"is from"+${x.hometown}`);
+   arrayOBJ.push(`${element.name} is from ${element.hometown}`);
   }
   
 return arrayOBJ;
